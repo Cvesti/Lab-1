407428825c5b43523170e4a8df06459d9d473bbf
crazivan8@gmail.com <crazivan8@gmail.com>
2024-10-02 22:58:43 +0300

    add 3ex

--- lab.py
+++ lab.py
@@ -1,21 +1,54 @@
-width = 40  
-height = 20  
-circle_radius = 6  
+import numpy as np
+
+#флаг бангладеша
+width = 22  
+height = 11  
+circle_radius = 3  
 circle_center = (width // 2 - 5, height // 2)  
 
 def in_circle(x, y, center, radius):
     return (x - center[0])**2 + (y - center[1])**2 <= radius**2
 
-flag_output = []
 for y in range(height):
-    row = []
     for x in range(width):
         if in_circle(x, y, circle_center, circle_radius):
-            row.append("@")  
+            print("\033[41m \033[31m", end="")
         else:
-            row.append(" ")  
-    flag_output.append("".join(row))
+            print("\033[42m \033[0m", end="")
+    print()
 
-output_str = "\n".join(flag_output)
-output_str
 
+#график функции y=2x+3
+
+x_values = np.arange(0, 6, 1)
+y_values = 2 * x_values + 3  
+
+height = 9
+width = 6
+
+graph = [[' ' for _ in range(width)] for _ in range(height)]
+
+for i in range(height):
+    graph[i][0] = '|'
+for j in range(width):
+    graph[height - 1][j] = '_'
+
+for x, y in zip(x_values, y_values):
+    if y < height: 
+        graph[height - 1 - y][x] = 'x'
+
+
+color_reset = "\033[0m"
+color_axis = "\033[34m"  
+color_point = "\033[32m"  
+
+#вывод на график
+for line in graph:
+    output_line = ''.join(line)
+    output_line = output_line.replace('|', f'{color_axis}|{color_reset}')
+    output_line = output_line.replace('-', f'{color_axis}-{color_reset}')
+    output_line = output_line.replace('x', f'{color_point}x{color_reset}')
+    print(output_line)
+
+
+print('012345') 
