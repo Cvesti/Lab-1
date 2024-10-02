33a3ab20563dd30beda9bc4485a6757174a4d81c
crazivan8@gmail.com <crazivan8@gmail.com>
2024-10-02 22:35:07 +0300

    hash del

--- lab.py
+++ lab.py
@@ -1,24 +1,21 @@
-# Размеры флага
-width = 40  # ширина
-height = 20  # высота
-circle_radius = 6  # радиус круга
-circle_center = (width // 2 - 5, height // 2)  # центр круга
+width = 40  
+height = 20  
+circle_radius = 6  
+circle_center = (width // 2 - 5, height // 2)  
 
-# Функция проверки, находится ли точка внутри круга
 def in_circle(x, y, center, radius):
     return (x - center[0])**2 + (y - center[1])**2 <= radius**2
 
-# Построение флага
 flag_output = []
 for y in range(height):
     row = []
     for x in range(width):
         if in_circle(x, y, circle_center, circle_radius):
-            row.append("@")  # Красный круг
+            row.append("@")  
         else:
-            row.append(" ")  # Зелёный фон
+            row.append(" ")  
     flag_output.append("".join(row))
 
-# Соединение строк для вывода
 output_str = "\n".join(flag_output)
 output_str
+
